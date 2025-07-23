1c6c3f54d678ddfdec369dea4a25632c008de402
lawsyhorsy <lawsonwrigley@gmail.com>
2025-07-23 14:24:48 +1000

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,13 @@
+import { GameBoard } from './components/GameBoard';
+
+const gameBoard = new GameBoard();
+
+function initGame() {
+    gameBoard.render();
+}
+
+function handleUserInteraction() {
+    // Logic to handle user interactions will go here
+}
+
+initGame();
